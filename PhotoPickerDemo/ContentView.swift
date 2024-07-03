3d73920d34b680b954f54276ce6200b7a28898bd
Tom Dobson <dobson980@gmail.com>
2024-07-03 14:14:18 -0700

    Initial Commit

---
+++ PhotoPickerDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoPickerDemo
+//
+//  Created by Thomas Dobson on 7/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoPickerDemo/PhotoPickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerDemoApp.swift
+//  PhotoPickerDemo
+//
+//  Created by Thomas Dobson on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
